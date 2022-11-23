f045bf4fe08f559b05a95836ed45429c0bd81b1d
Paula A. B. Rodrigues <coder@paulaabro.tech>
2022-11-22 22:49:47 -0300

    variables

---
+++ lec_04/code_01/index.js
@@ -0,0 +1,18 @@
+var num1 = 1;
+let num2 = 2;
+const num3 = 3;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log();
+
+num1 = 10;
+num2 = 20;
+
+// TypeError;
+// num3 = 30;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
