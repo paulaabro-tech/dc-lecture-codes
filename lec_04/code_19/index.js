a630efee9b231e187f67b40c66fd95e9d35bd445
Paula A. B. Rodrigues <coder@paulaabro.tech>
2022-11-24 01:26:49 -0300

    for loop

---
+++ lec_04/code_19/index.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
+console.log("The END!");
+console.log();
+
+const names = ["Paula", "Mirilaine", "Charles", "Wagner", "Fernanda"];
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
