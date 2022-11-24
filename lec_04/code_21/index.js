9fbc864e85ce17230018f743fa8f77b01ad87e39
Paula A. B. Rodrigues <coder@paulaabro.tech>
2022-11-24 01:37:25 -0300

    for of

---
+++ lec_04/code_21/index.js
@@ -0,0 +1,12 @@
+const firstName = "Paula";
+const names = ["Paula", "Mirilaine", "Charles", "Wagner", "Fernanda"];
+
+for (const letter of firstName) {
+  console.log(letter);
+}
+
+console.log();
+
+for (const name of names) {
+  console.log(name);
+}
