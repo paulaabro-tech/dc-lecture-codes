09945b08937bf6c88e976f43db215d8117b36a5a
Paula A. B. Rodrigues <coder@paulaabro.tech>
2022-11-24 05:49:06 -0300

    functions intro, parameter and arguments, and return

---
+++ lec_05/code_02/index.js
@@ -0,0 +1,10 @@
+function sum(num1, num2) {
+  console.log(num1 + num2);
+}
+
+sum(3, 7);
+
+// ReferenceError
+// console.log(num1);
+
+sum(21, 6);
---
+++ lec_05/code_03/index.js
@@ -0,0 +1,10 @@
+function displaySum(num1, num2) {
+  console.log(num1 + num2);
+}
+
+function sum(num1, num2) {
+  return num1 + num2;
+  console.log("Never gets here 😭");
+}
+console.log(`The result is: ${displaySum(3, 7)}`);
+console.log(`The result is: ${sum(3, 7)}`);
