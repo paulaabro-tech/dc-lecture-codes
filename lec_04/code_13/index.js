80a1303ba09a47bae6d24446e5e948a5215736bc
Paula A. B. Rodrigues <coder@paulaabro.tech>
2022-11-23 22:25:47 -0300

    logical operators

---
+++ lec_04/code_13/index.js
@@ -0,0 +1,17 @@
+console.log(`true && true = ${true && true}`);
+console.log(`true && false = ${true && false}`);
+console.log(`false && true = ${false && true}`);
+console.log(`false && false = ${false && false}`);
+console.log();
+
+console.log(`true || true = ${true || true}`);
+console.log(`true || false = ${true || false}`);
+console.log(`false || true = ${false || true}`);
+console.log(`false || false = ${false || false}`);
+console.log();
+
+console.log(`!true = ${!true}`);
+console.log(`!false = ${!false}`);
+console.log();
+
+console.log(!(!(true && true && true) && !((false && true) || true)));
